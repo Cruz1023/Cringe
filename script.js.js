00bfc0d9c46457f6da8e672b020c1cf106703db1
Cruz1023 <137800032+Cruz1023@users.noreply.github.com>
2026-07-15 16:25:25 -0500

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,9 @@
+function abrirCarta() {
+
+    const inicio = document.getElementById("inicio");
+    const carta = document.getElementById("carta");
+
+    inicio.style.display = "none";
+    carta.style.display = "flex";
+
+}
